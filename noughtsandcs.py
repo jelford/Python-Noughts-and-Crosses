dac98f4cb4bd379e32d5d0df4eb2842a8dc493a5
James Elford <james.elford08@imperial.ac.uk>
2010-11-16 19:12:10 +0000

    Added a printboard function

--- noughtsandcs.py
+++ noughtsandcs.py
@@ -9,5 +9,12 @@ ROWS = 3
 # Initialize the board
 for letter in range(ROWS):
   for number in range(1,COLS+1):
-    board[alphabet[letter] + str(number)] = " "
+    board[alphabet[letter] + str(number)] = "."
 
+def printBoard(board) :
+  print ("\n==========")
+  print "   1  2  3"
+  print ("a  " + board["a1"] + "  " + board["a2"] + "  " + board["a3"])
+  print ("b  " + board["b1"] + "  " + board["b2"] + "  " + board["b3"])
+  print ("c  " + board["c1"] + "  " + board["c2"] + "  " + board["c3"])
+  print ("==========")
